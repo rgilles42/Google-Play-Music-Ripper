e91c4a77bf1521ffef5cb75aac0afa7b0cfb3c8d
Unknown <PascalRoose@outlook.com>
2019-06-11 22:12:56 +0200

    track_downloader now uses the correct import
    Couldn't change the import statement last commit. Doing that caused git to think it's a new file rather than being a file that's moved and edited

--- examples/track_downloader.py
+++ examples/track_downloader.py
@@ -1,6 +1,6 @@
-import func
+from context import gmusicripper
 
-client = func.performoauth()
+client = gmusicripper.performoauth()
 access_token = client.session._oauth_creds.access_token
-func.get_mp3(func.search_storeid_track(input("Enter keywords (Title, Artist...) of the song you'd like to download: "),
+gmusicripper.get_mp3(gmusicripper.search_storeid_track(input("Enter keywords (Title, Artist...) of the song you'd like to download: "),
                                        access_token), access_token, 'Fetched')
